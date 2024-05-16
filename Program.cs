fd6dc45e905d341b4c2fc74d1b6e27b253eb401a
Justin Hainline <Justin.Hainline@mssa.newhorizons.com>
2024-05-16 09:07:07 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Challenge_Lab_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string correctUserId = "Justin Hainline";
+            const string correctPassword = "mypassword";
+
+            int attempts = 0;
+
+            while (attempts < 3)
+            {
+                Console.WriteLine("Enter user ID: ");
+                string userId = Console.ReadLine();
+
+                Console.WriteLine();
+
+                Console.WriteLine("Enter password: ");
+                string password = Console.ReadLine();
+
+                Console.WriteLine();
+
+                if (userId == correctUserId && password == correctPassword) 
+                {
+                    Console.WriteLine("Authentication successfull!") ;
+                    break;
+                }
+
+                else
+                {
+                    attempts++;
+                    Console.WriteLine($"Incorrect credentials. Attempts remaining: {3 - attempts}");
+                    Console.WriteLine($"Account Locked. {0 == attempts}");
+                }
+
+                
+            }
+        }
+    }
+}
